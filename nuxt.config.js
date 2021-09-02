e9260ed2e276fcb8cbadb55d9154bb0f4705ce81
Edouard Yonga <ededwardyonga@gmail.com>
2021-09-02 15:30:22 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  target: 'static',
+  srr:false,
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         titleTemplate: '%s -',
